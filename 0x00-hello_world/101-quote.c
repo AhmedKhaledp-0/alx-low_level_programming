f8629c674a7ef65363dcf74f051cc62f01d5a89f
=brokeninfp <=brokeninfp@gmail.com>
2023-02-16 17:51:19 -0800

    alx-low_level_programming task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -9,6 +10,6 @@
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
