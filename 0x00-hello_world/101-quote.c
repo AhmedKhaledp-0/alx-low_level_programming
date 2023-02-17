14edf3510f6bc91d3aaeccf42d2706faebc7c713
=brokeninfp <=brokeninfp@gmail.com>
2023-02-16 17:46:14 -0800

    alx-low_level_programming task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Write a C program that prints exactly and that piece of art is useful'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
