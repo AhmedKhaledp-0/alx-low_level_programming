266135012002c9b8cfbced7f862d4258e50d13cd
=brokeninfp <=brokeninfp@gmail.com>
2023-02-16 17:00:02 -0800

    alx-low_level_programming task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'that is alx'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alxee[] = "\"Programming is like building a multilingual puzzle";
