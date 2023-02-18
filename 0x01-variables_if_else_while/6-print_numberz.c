a972ec5e44d19e0a70013ccfac7b3b12e575baa8
=brokeninfp <=brokeninfp@gmail.com>
2023-02-17 18:43:50 -0800

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * mian - print numbers 0:9
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n='0';n <= '9'; n++)
+	{
+		putchat(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
