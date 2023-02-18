446432fb253fb8635f8e4137f68ad735d519dd15
=brokeninfp <=brokeninfp@gmail.com>
2023-02-17 18:59:17 -0800

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
-* main -The last digit 
-*
-* Return: Always (Success)
-*/
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-        int n, lastd;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
 
 	if (lastd > 5)
@@ -28,5 +27,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
+
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the alphabet at reverse
+  * main - Prints a hexadecimal string
   *
   * Return: Always (Success)
   */
@@ -9,7 +9,12 @@ int main(void)
 {
 	char c;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
