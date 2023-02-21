c469e8c58c8b25f16893119c221e49c0d40e47bb
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 10:28:24 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: '0x02. C - Functions, nested loops'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i ='a'; i <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
