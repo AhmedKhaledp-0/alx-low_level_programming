55cbed089067b0aca9948c19e0b43277b27946c6
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 18:20:49 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @x: first number input
+ * @y: second number input
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	int z ;
+	
+	z = x+y;
+	return (z);
