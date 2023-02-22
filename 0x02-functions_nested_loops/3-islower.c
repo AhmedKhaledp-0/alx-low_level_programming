cea8a92296feb771d0fb159df4fdb0e108aa68aa
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 16:56:16 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _islower - Entry point
+ * @c: single letter input
  * Description: '0x02. C - Functions, nested loops'
- * Return:1 if int c is lowercase, 0 if otherwise 
+ * Return:1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
 {
