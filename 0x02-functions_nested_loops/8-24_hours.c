ad55d17566dab082e4ef366399f0e199b8131641
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 18:08:41 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /*
  * jack_bauer - function that prints every minute of the day
+ * Description: '0x02. C - Functions, nested loops'
  * h = hour, m = minutes
  * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
