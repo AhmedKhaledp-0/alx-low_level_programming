cbfe63a45bcaf1862732dbb7bd7e7f1be77f9681
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 17:23:45 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,9 +8,17 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		return ('+');
+	{
+		printf ('+');
+		return (1);
+	}
 	else if (n < 0)
-		return ('-');
+	{
+		printf ('-');
+		return (-1);
 	else
+	{
+		printf ('0');
 		return (0);
+	}
 }
