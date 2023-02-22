381bf02f903d2b9799fd131d3ac087f46c5ebbc2
=brokeninfp <=brokeninfp@gmail.com>
2023-02-21 17:51:49 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @dig: number's last digit result
+ * Return: value of the last digit
+ */
+int print_last_digit(int dig)
+{
+	int i;
+	i = (dig % 10);
+	if (i < 0)
+	{
+		dig = (-1 * i);
+	}
+	_putchar(i + '0');
+	return (i);
+}
