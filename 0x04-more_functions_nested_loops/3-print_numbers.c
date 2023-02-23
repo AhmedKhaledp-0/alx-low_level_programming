274adceaebafa4a85fe3cb5df0ba0186e2b339c9
=brokeninfp <=brokeninfp@gmail.com>
2023-02-23 15:14:25 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,10 @@
  */
 void print_numbers(void)
 {
-	for (int i = 0 ; i < 9; i++)
+	int i;
+	for (i = 0 ; i < 9; i++)
 	{
-		_putchar ('i')
+		_putchar ('i');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
