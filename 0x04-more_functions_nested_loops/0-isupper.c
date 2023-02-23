312a31d069893e9cbcc4df87ca5beee447503239
=brokeninfp <=brokeninfp@gmail.com>
2023-02-23 14:43:33 -0800

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper(int c) - check uppercase
+ * Return : 1 if uppercase 0 if otherwice
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}	
