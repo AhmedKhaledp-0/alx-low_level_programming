50bb58dcdb6f2d8ef0fc17da16922d5880b3c15d
=brokeninfp <=brokeninfp@gmail.com>
2023-02-23 15:29:30 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- * Return: Nothing
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Void
  */
 void print_numbers(void)
 {
