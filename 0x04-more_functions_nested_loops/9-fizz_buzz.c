b23645f00ed666af85762d632c1fa02f740b1ff8
=brokeninfp <=brokeninfp@gmail.com>
2023-02-23 17:45:43 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,13 +2,14 @@
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
-		if (i %15 == 00)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		if (i %3 == 0)
+		if (i % 3 == 0)
 			printf("Fizz");
-		else if (i %5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%i",i);
