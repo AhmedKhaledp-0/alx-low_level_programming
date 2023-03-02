f8b27d9375ad12e30edc8e40d40b189e8bf9c348
=brokeninfp <=brokeninfp@gmail.com>
2023-03-02 14:50:18 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * char *_strcat(char *dest, char *src) - main function
- * @*dest: first pointer 
+ * char *_strcat - main function
+ * @*dest: first pointer
  * @*src: second pointer
  * RETURN: dest_star
  */
 char *_strcat(char *dest, char *src)
 {
-	 char *dest_start = dest;
-	 while (*dest != '\0')
-	 {
-        	dest++;
-	 }
-	 while (*src != '\0')
-	 {
-        	*dest = *src;
-       		dest++;
-        	src++;
-	 }
-	 *dest = '\0';
-	 return dest_start;
+	char *dest_start = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest_start);
 }
