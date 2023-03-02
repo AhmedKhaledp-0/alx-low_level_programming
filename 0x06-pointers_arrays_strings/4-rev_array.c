2b0f2cadc8ab34203c185b0086030c306c365d0d
=brokeninfp <=brokeninfp@gmail.com>
2023-03-02 15:35:51 -0800

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * reverse_array - main function
+ * @a: int 
+ * @b: int 
+ * Return: always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+
+	for (int i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
