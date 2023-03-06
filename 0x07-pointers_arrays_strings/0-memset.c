e989c0d190c23a84e7e91da49b8902389f4bbae6
=brokeninfp <=brokeninfp@gmail.com>
2023-03-06 15:23:07 -0800

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset -  function that fills memory with a constant byte.
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (S);
+}	
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x07. C - Even more pointers, arrays and strings
+## C - Pointer to Pointers
+## C – Pointer to Pointer with example
+## Multi-dimensional Arrays in C
+## Two dimensional (2D) arrays in C programming with example
+
