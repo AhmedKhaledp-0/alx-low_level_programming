147a1164faf8a1431798dc88bb8097811473f384
=brokeninfp <=brokeninfp@gmail.com>
2023-03-08 17:47:45 -0800

    no loops

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number
+ * @n: int
+ * Return: factors
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
