1a49fccbbb865a484977aa5c3df6f7059fc40df2
=brokeninfp <=brokeninfp@gmail.com>
2023-03-08 17:20:02 -0800

    no loops

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: a pointer for char
+ * Return: nt
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s - 1);
+}
