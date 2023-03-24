bc9884c6ed7d871f429baac7826678d94ddfda7b
=brokeninfp <=brokeninfp@gmail.com>
2023-03-23 18:37:35 -0700

    error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * @n: The number of paramters passed to the function.
  * @...: A variable number of paramters to calculate the sum of.
  * Return: If n == 0 - 0.
- * */
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
