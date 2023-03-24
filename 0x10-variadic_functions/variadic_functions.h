b3e22f3246f77cdf66c6036807e9bc81036deb47
=brokeninfp <=brokeninfp@gmail.com>
2023-03-23 18:19:39 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,28 @@
-ifndef _variadic_
-#define _variadic_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#define null (void *)0
+#define TRUE 1
+#define FALSE 0
+#define is ==
+#include <stdarg.h>
+
+
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
 
+#endif
