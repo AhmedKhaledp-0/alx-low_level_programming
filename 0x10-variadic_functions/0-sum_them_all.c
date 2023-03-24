f84e1c9e1caf7e1bd5d9fa6dbeaf91f01204bd48
=brokeninfp <=brokeninfp@gmail.com>
2023-03-23 18:29:35 -0700

    error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,19 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
- * sum_them_all - returns the sum of all parameters
- * @n: number of parameters
- * Return: sum of parameters
- */
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ * Return: If n == 0 - 0.
+ * */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	unsigned int i;
-	va_list args;
-
-	va_start(args, n);
+	va_list ap;
+	unsigned int i, sum = 0;
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-	va_end(args);
+		sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 }
+
